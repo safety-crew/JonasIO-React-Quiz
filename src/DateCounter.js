1a00d515a1f06fc2d416a7bf610dd5c91d33c140
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-12-03 14:31:05 +0200

    Add Step and Reset States to Reducer Function

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,15 +1,21 @@
 import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
+
 function reducer(state, action) {
   console.log(state, action);
 
   switch (action.type) {
     case "dec":
-      return { ...state, count: state.count - 1 };
+      return { ...state, count: state.count - state.step };
     case "inc":
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + state.step };
     case "setCount":
       return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
 
     default:
       throw new Error("Unknown action");
@@ -32,7 +38,6 @@ function DateCounter() {
   // const [count, setCount] = useState(0);
   // const [step, setStep] = useState(1);
 
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
@@ -59,9 +64,11 @@ function DateCounter() {
 
   const defineStep = function (e) {
     // setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
+    dispatch({ type: "reset" });
     // setCount(0);
     // setStep(1);
   };
