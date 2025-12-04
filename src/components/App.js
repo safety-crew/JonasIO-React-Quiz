d09e5c0e268c1323699fb842ba5b5b4e92a8d235
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-12-04 20:34:00 +0200

    Add Restart Quiz Logic

--- src/components/App.js
+++ src/components/App.js
@@ -47,6 +47,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
 
     default:
       throw new Error("Action Unkown");
@@ -108,6 +110,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
