1dcfa6180b4241d69a9b92d72dc7f52c88a25ce3
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-12-04 17:48:09 +0200

    Handle new Answers & Add moving to next question logic

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NexButton";
 
 const initialState = {
   questions: [],
@@ -12,6 +13,8 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -22,6 +25,18 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
 
     default:
       throw new Error("Action Unkown");
@@ -29,7 +44,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -53,7 +68,17 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
---
+++ src/components/NexButton.js
@@ -0,0 +1,16 @@
+function NexButton({ dispatch, answer }) {
+  if (answer === null) {
+    return null;
+  }
+
+  return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: "nextQuestion" })}
+    >
+      Next
+    </button>
+  );
+}
+
+export default NexButton;
